ac31595d1ad9a4d27df5cfa6c4414ac2645c4066
Yaraslau Dzenisiuk <cylinder.y@gmail.com>
2019-10-07 23:24:27 +0300

    task 2.1 creating shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 
 import { ProductsModule } from './products/products.module';
 import { CartModule } from './cart/cart.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { CartModule } from './cart/cart.module';
     BrowserModule,
     AppRoutingModule,
     ProductsModule,
-    CartModule
+    CartModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
